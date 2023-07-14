cd0aa3c8aaabde9b135ef125f9699c8fdf4a4399
FreemindNation <freemindnation@outlook.com>
2023-07-14 22:32:04 +0100

    Create calculator.js index.html and style.css file
    Edit calculator.js file to create basic math operators functions.

---
+++ calculator.js
@@ -0,0 +1,10 @@
+const Add = (num1, num2) => (num1 + num2);
+const Subtract = (num1, num2) => (num1 - num2);
+const Multiply = (num1, num2) => (num1 * num2);
+const Divide = (num1, num2) => (num1 / num2);
+
+
+console.log(Add(1, 2));
+console.log(Subtract(1, 2));
+console.log(Multiply(1, 2));
+console.log(Divide(1, 2));
