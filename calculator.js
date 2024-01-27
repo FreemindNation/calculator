b1203fc2cd0bd97db272bd4269f691cad7fb1120
FreemindNation <freemindnation@outlook.com>
2024-01-27 16:25:33 +0000

    Edit calculator.js index.html style.css

--- calculator.js
+++ calculator.js
@@ -10,21 +10,20 @@ let secondNumber = "";
 let total = 0;
 
 const operate = (firstNum, secondNum, operator) => {
-    // firstNumber = parseFloat(firstNum);
-    // secondNumber = parseFloat(secondNum);
-    if (operator === "add") {
+    
+    if (operator === "+") {
          total = add(parseFloat(firstNum), parseFloat(secondNum));
         display.textContent = parseFloat(total.toFixed(6));
     }
-    else if(operator === "subtract") {
+    else if(operator === "-") {
         total = subtract(parseFloat(firstNum), parseFloat(secondNum));
         display.textContent = parseFloat(total.toFixed(6));
     }
-    else if(operator === "multiply") {
+    else if(operator === "*") {
         total = multiply(parseFloat(firstNum), parseFloat(secondNum));
         display.textContent = parseFloat(total.toFixed(6));
     }
-    else if(operator === "divide") {
+    else if(operator === "/") {
         if(secondNum === "0") {
             display.textContent = "Cannot divide by zero!";
         }
@@ -33,7 +32,7 @@ const operate = (firstNum, secondNum, operator) => {
         display.textContent = parseFloat(total.toFixed(6));
         }
     }
-    else if(operator === "modulo") {
+    else if(operator === "%") {
         total = modulo(parseFloat(firstNum), parseFloat(secondNum));
         display.textContent = parseFloat(total.toFixed(6));
     }
@@ -50,9 +49,11 @@ const decimal = document.querySelector("#decimal");
 const opposite = document.querySelector("#opposite");
 const operators = document.querySelectorAll(".operator");
 const equals = document.querySelector(".equals");
+const currentYear = Document.querySelector("#year");
+
+currentYear.textContent = new Date().getFullYear();
 
 let storedValue = "";
-// let operatorsArr = ["+", "-", "*","/","%"];
 
 const displayValue = (number) => {
     const inputNumber = display.textContent;
@@ -60,10 +61,7 @@ const displayValue = (number) => {
         display.textContent = parseFloat(inputNumber + number).toString();
     }
 }
-// if (display.textContent.length > 12) {
-//     display.textContent.style.fontSize = "1em";
-//     console.log(display.textContent);
-// }
+
 digits.forEach(digit => digit.addEventListener("click", () => {
 
     displayValue(digit.id);
@@ -130,19 +128,12 @@ decimal.addEventListener("click", addDecimal);
 opposite.addEventListener("click", reverseNumber);
 
 
-// multiplyOpp.addEventListener("click", () => {
-//     miniDisplay.textContent = display.textContent + " " + multiplyOpp.value;
-    
-// })
-
-
-
-// let storedValuesArr = [];
 let clickedOperatorArr = [];
 let currentOperator = "";
 let previousOperator = "";
+
 operators.forEach(currOpp => currOpp.addEventListener("click", ()=> {
-    // operator = currOpp.dataset.action;
+    
     operator = currOpp.id;
     display.textContent = "";
     clickedOperatorArr += operator;
@@ -155,60 +146,17 @@ operators.forEach(currOpp => currOpp.addEventListener("click", ()=> {
       for(let i= 0; i < clickedOperatorArr.length; i++) {
         currentOperator = clickedOperatorArr[i];
         previousOperator = clickedOperatorArr[i - 1];
-
       }
-        // console.log(secondNumber);
       operate(firstNumber, secondNumber, previousOperator);
       firstNumber = display.textContent;
       miniDisplay.textContent = firstNumber + " " + currentOperator;
-        // clickedOperatorArr.slice(1);
       display.textContent = "";
       
       
-      // removeFirstOperator(clickedOperatorArr);
       console.log(clickedOperatorArr);  
     }
-    // else if(clickedOperatorArr.length > 2) {
-    //   clickedOperatorArr.slice(1);
-    //     console.log(clickedOperatorArr);
-    // }
-    
-
-    // if(display.textContent !== "" || isNaN(display.textContent)) {
-    //     secondNumber = display.textContent;
-    //     console.log(secondNumber);
-    //      operate(firstNumber, secondNumber, operator);
-    // }
-    // storedValues += currOpp.id;
-    // let regex = /\d+\.\d+|\d+|[^0-9]/g;
-    // let storedValuesStr = storedValues.match(regex);
-    // let storedValuesArr = storedValuesStr.split(" ");   
-    // chainOp();
-    // console.log(storedValue);
-    // secondNumber = display.textContent;
-    
-    
-    // operate(firstNumber, secondNumber, operator);
-
-    // console.log(secondNumber);
-    
-    // console.log(firstNumber);
-
-
 }))
 
-
-// console.log(storedValuesArr);
-//  const chainOp = ()=> {
-//     for (let i = 0; i < storedValuesArr.length; i++) {
-//         if(operatorsArr.includes(storedValuesArr[i])) {
-//             operator = storedValuesArr[i];
-//             firstNumber = storedValuesArr[i - 1];
-//             secondNumber = storedValuesArr[i + 1];
-//             operate(firstNumber, secondNumber, operator);
-//         }
-//     }
-//  }
 equals.addEventListener("click", () => {
     if(operator !== "") {
         secondNumber = display.textContent;
@@ -217,23 +165,5 @@ equals.addEventListener("click", () => {
     }
     
     miniDisplay.textContent += " " + secondNumber + " " + "=" + " ";
-    
-
-    // console.log(operator);
-    // console.log(firstNumber, secondNumber);
 })
-
-
-
-// const nine = document.querySelector("#9");
-// const eight = document.querySelector("#8");
-// const seven = document.querySelector("#7");
-// const six = document.querySelector("#6");
-// const five = document.querySelector("#5");
-// const four = document.querySelector("#4");
-// const three = document.querySelector("#3");
-// const two = document.querySelector("#2");
-// const one = document.querySelector("#1");
-// const zero = document.querySelector("#0");
-// const plusMinus = document.querySelector("#-");
 
