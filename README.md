4b321a07303e583a2f82628830a06a013cc43169
FreemindNation <freemindnation@outlook.com>
2024-01-27 22:38:53 +0000

    Edit calculator.js and README.md for final look.
    Remove all console.logs from calculator.js
    Add comments to calculator.js

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-This is a calculator project as part of the Odin Project curriculum. I will use javaScript, HTML and CSS  to complete the project. more details will be added as i go along
+This is a calculator project as part of the Odin Project curriculum. I will use javaScript, HTML and CSS  to complete the project. more details will be added as i go along.
+
+The project is complete. It has three main files. a javaScript file function, a HTML file for design and a CSS file for styling.
+
+The calculator is simple but has the capacity to perform all numerical operations with two or multiple operands at the same time.
+
+The calculator also allows the use of floating point numbers as well as negative numbers.
+
+for more information please refer to he code in the files.
+
--- calculator.js
+++ calculator.js
@@ -1,14 +1,17 @@
+//functions for the operations.
 const add = (num1, num2) => (num1 + num2);
 const subtract = (num1, num2) => (num1 - num2);
 const multiply = (num1, num2) => (num1 * num2);
 const divide = (num1, num2) => (num1 / num2);
 const modulo = (num1, num2) => (num1 % num2);
 
+// variables declared to store the operands.
 let firstNumber = "";
 let operator = "";
 let secondNumber = "";
 let total = 0;
 
+// function that runs the operation using the operands.
 const operate = (firstNum, secondNum, operator) => {
     
     if (operator === "+") {
@@ -37,7 +40,7 @@ const operate = (firstNum, secondNum, operator) => {
         display.textContent = parseFloat(total.toFixed(6));
     }
 }
-
+// DOM elements
 const calculator = document.querySelector(".calculator");
 const display =  document.querySelector(".display");
 const miniDisplay = document.querySelector(".mini-display");
@@ -49,38 +52,36 @@ const decimal = document.querySelector("#decimal");
 const opposite = document.querySelector("#opposite");
 const operators = document.querySelectorAll(".operator");
 const equals = document.querySelector(".equals");
-const currentYear = Document.querySelector("#year");
+const currentYear = document.querySelector("#year");
 
-currentYear.textContent = new Date().getFullYear();
+currentYear.textContent = new Date().getFullYear() + " ";  //current year in the footer.
 
 let storedValue = "";
 
+//function to populate the main display every time a number is clicked.
 const displayValue = (number) => {
     const inputNumber = display.textContent;
     if (inputNumber.length < 16) {
         display.textContent = parseFloat(inputNumber + number).toString();
     }
 }
-
+// event listener for every number button.
 digits.forEach(digit => digit.addEventListener("click", () => {
 
     displayValue(digit.id);
     if(operator === "") {
         storedValue = display.textContent;
         firstNumber = storedValue;
-        console.log(firstNumber);
     }
     
     else if(operator !== "") {
         
         storedValue = display.textContent;
         secondNumber = storedValue;
-
     }
-    
-    console.log(firstNumber, secondNumber);
-})    
-);
+}));
+
+//function to clear all data
 const clearDisplay =() => { 
     display.textContent = 0;
     miniDisplay.textContent ="";
@@ -92,6 +93,7 @@ const clearDisplay =() => {
     previousOperator = "";
     clickedOperatorArr = [];
 }
+//function to remove last entered number
 const removeDigit = () => {
     if(display.textContent.length === 1){
         display.textContent = 0;
@@ -100,11 +102,13 @@ const removeDigit = () => {
     display.textContent = display.textContent.slice(0, display.textContent.length -1)
     }
 }
+//function to add a decimal .
 const addDecimal = () => {
      if(display.textContent.indexOf(".") === -1) {
         display.textContent += "."; 
     }
 }
+//function to add negative numbers.
 const reverseNumber = () => {
     if(operator === "") {
         display.textContent = display.textContent * -1;
@@ -113,10 +117,10 @@ const reverseNumber = () => {
     else {
         display.textContent = display.textContent * -1;
         secondNumber = display.textContent;
-    }
-    
+    }  
 }
 
+//event listeners to clear all data, remove last number, add decimal and negative numbers.
 clear.addEventListener("click" ,clearDisplay);
 
 backSpace.addEventListener("click", removeDigit);
@@ -127,11 +131,12 @@ decimal.addEventListener("click", addDecimal);
 
 opposite.addEventListener("click", reverseNumber);
 
-
+// variables declared for current and previous operators as well as array to store them in.
 let clickedOperatorArr = [];
 let currentOperator = "";
 let previousOperator = "";
 
+// event listener for every operator button.
 operators.forEach(currOpp => currOpp.addEventListener("click", ()=> {
     
     operator = currOpp.id;
@@ -150,13 +155,11 @@ operators.forEach(currOpp => currOpp.addEventListener("click", ()=> {
       operate(firstNumber, secondNumber, previousOperator);
       firstNumber = display.textContent;
       miniDisplay.textContent = firstNumber + " " + currentOperator;
-      display.textContent = "";
-      
-      
-      console.log(clickedOperatorArr);  
+      display.textContent = ""; 
     }
-}))
+}));
 
+// event listener for the equal button.
 equals.addEventListener("click", () => {
     if(operator !== "") {
         secondNumber = display.textContent;
@@ -165,5 +168,5 @@ equals.addEventListener("click", () => {
     }
     
     miniDisplay.textContent += " " + secondNumber + " " + "=" + " ";
-})
+});
 
